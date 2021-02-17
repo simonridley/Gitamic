6f30a180f44ec36bd4f24a9c619945cb870224da
Simon Hamp <simon.hamp@me.com>
2021-02-17 19:24:36 +0000

    Fix some content links

--- src/Repository.php
+++ src/Repository.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\File;
 use Gitonomy\Git\Diff\File as GitFile;
-use Barryvdh\Debugbar\Facade as Debugbar;
 use Gitonomy\Git\Repository as GitRepository;
 
 class Repository implements Contracts\SiteRepository
@@ -78,17 +77,25 @@ class Repository implements Contracts\SiteRepository
     protected function getFileDetails($relative_path, $id): Collection
     {
         $path = base_path($relative_path);
+        $file = File::name($path);
         $extension = '.' . File::extension($path);
         $last_modified = Carbon::createFromTimestamp(File::lastModified($path))->format('Y-m-d H:i:s');
         $type = File::type($path);
         $size = File::size($path);
-        $is_content = Str::startsWith($path, app('filesystems.paths.content'));
+        $is_content = empty($file) ? false : Str::startsWith($path, app('filesystems.paths.content'));
         $entry_path = Str::replaceFirst('content' . DIRECTORY_SEPARATOR, '', $relative_path);
+
+        // Replace taxonomy term path with the real deal
+        $entry_path = preg_replace('#taxonomies/(.*?)/(.*)\.yaml#', 'taxonomies/$1/terms/$2/default', $entry_path);
+
+        $clean_path = Str::replaceLast($extension, '', $entry_path);
+        $edit_url = null;
+
+        if ($is_content) {
+            $edit_url = implode(DIRECTORY_SEPARATOR, [config('statamic.cp.route'), $clean_path]);
+        }
+
         $collection = explode(DIRECTORY_SEPARATOR, $entry_path)[1] ?? null;
-        $edit_url = DIRECTORY_SEPARATOR
-            . config('statamic.cp.route')
-            . DIRECTORY_SEPARATOR
-            . Str::replaceLast($extension, '', $entry_path);
 
         $is_entry = false;
         try {
