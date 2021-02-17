66b2120144c65c7c6797d09e43ea06a9e601d7ff
Simon Hamp <simon.hamp@me.com>
2021-02-17 19:24:51 +0000

    Update the contract

--- src/Contracts/SiteRepository.php
+++ src/Contracts/SiteRepository.php
@@ -2,7 +2,25 @@
 
 namespace SimonHamp\Gitamic\Contracts;
 
+use Illuminate\Support\Collection;
+
 interface SiteRepository
 {
+    public function getFilesOfType($type): Collection;
 
+    public function getUnstagedFiles(): Collection;
+
+    public function getUntrackedFiles(): Collection;
+
+    public function getStagedFiles(): Collection;
+
+    public function getPendingFiles(): Collection;
+
+    public function stage($files, $args = []);
+
+    public function unstage($files, $args = []);
+
+    public function remove($files, $args = []);
+
+    public function commit($message);
 }
